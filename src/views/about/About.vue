203d22cb8841926d71a704a354a6091a8a65b203
linhexs <838311829@qq.com>
2020-02-10 22:51:27 +0800

    2020/2/10 完善首页

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -3,48 +3,57 @@
     <div class="lin-info">
       <div class="lin-info-left">
         <div class="welcome">
-          <div class="welcome-title"></div>
+          <div class="welcome-title">欢迎来到微信小程序商城CMS</div>
           <div class="subtitle">
             <div class="guide"></div>
-            <div class="link"><a href="http://www.7yue.pro" target="_blank">http://www.7yue.pro</a></div>
+            <div class="link">
+              <a href="http://mall.linhe3306.com" target="_blank">访问另一个pc端项目</a>
+            </div>
           </div>
         </div>
-        <img class="welcome-bg" src="../../assets/img/about/header-bg.png" alt="" />
+        <img class="welcome-bg" src="../../assets/img/about/header-bg.png" alt />
       </div>
       <div class="lin-info-right">
         <div class="team-detail">
           <div class="team-box">
-            <div class="team-title">产品团队</div>
+            <div class="team-title">产品技术</div>
             <ul class="team-ul">
               <li>
-                <span class="shadow-box"> <i class="team-shadow"></i> </span> <span class="team-role">策划</span>
-                <span class="team-name">七月</span>
+                <span class="shadow-box">
+                  <i class="team-shadow"></i>
+                </span>
+                <span class="team-role">用户端</span>
+                <span class="team-name">微信小程序</span>
               </li>
               <li>
-                <span class="shadow-box"> <i class="team-shadow"></i> </span> <span class="team-role">研发</span>
+                <span class="shadow-box">
+                  <i class="team-shadow"></i>
+                </span>
+                <span class="team-role">CMS</span>
                 <span class="team-name">
                   <ul v-if="!showTeam">
-                    <li>Pedro</li>
-                    <li>一飞</li>
-                    <li>凉面</li>
-                    <li>圈圈</li>
-                    <li>家乐</li>
-                    <li>Jocky</li>
-                    <li>流乔</li>
+                    <li>Vue.js</li>
+                    <li>ThinkPHP</li>
+                    <li>Nginx</li>
                   </ul>
                   <ul v-else>
-                    <li>林间有风 CMS 组</li>
+                    <li>CMS</li>
                   </ul>
                 </span>
               </li>
               <li>
-                <span class="shadow-box"> <i class="team-shadow"></i> </span> <span class="team-role">设计</span>
-                <span class="team-name">瓜瓜</span>
+                <span class="shadow-box">
+                  <i class="team-shadow"></i>
+                </span>
+                <span class="team-role">负责人</span>
+                <span class="team-name">lin</span>
               </li>
             </ul>
           </div>
-          <div class="team-icon"><img src="../../assets/img/about/qrcode.jpg" alt="" /></div>
-          <p class="team-label">林间有风公众号</p>
+          <div class="team-icon">
+            <img src="../../assets/img/about/qrcode.jpg" alt />
+          </div>
+          <p class="team-label">扫码体验小程序</p>
         </div>
       </div>
     </div>
@@ -57,7 +66,9 @@
             <div class="quantity">11,590</div>
           </div>
         </div>
-        <div class="quantity-icon"><img src="../../assets/img/about/icon.png" alt="" /></div>
+        <div class="quantity-icon">
+          <img src="../../assets/img/about/icon.png" alt />
+        </div>
       </div>
       <div class="quantity-item">
         <div class="quantity-detail">
@@ -67,7 +78,9 @@
             <div class="quantity">51,862</div>
           </div>
         </div>
-        <div class="quantity-icon"><img src="../../assets/img/about/icon.png" alt="" /></div>
+        <div class="quantity-icon">
+          <img src="../../assets/img/about/icon.png" alt />
+        </div>
       </div>
       <div class="quantity-item">
         <div class="quantity-detail">
@@ -77,7 +90,9 @@
             <div class="quantity">1,862</div>
           </div>
         </div>
-        <div class="quantity-icon"><img src="../../assets/img/about/icon.png" alt="" /></div>
+        <div class="quantity-icon">
+          <img src="../../assets/img/about/icon.png" alt />
+        </div>
       </div>
       <div class="quantity-item">
         <div class="quantity-detail">
@@ -87,81 +102,17 @@
             <div class="quantity">1,323</div>
           </div>
         </div>
-        <div class="quantity-icon"><img src="../../assets/img/about/icon.png" alt="" /></div>
+        <div class="quantity-icon">
+          <img src="../../assets/img/about/icon.png" alt />
+        </div>
       </div>
     </div>
     <div class="information">
       <div class="personal">
-        <div class="personal-title">个人信息</div>
-        <img src="../../assets/img/about/avatar.png" class="personal-avatar" />
-        <div class="personal-influence">
-          <div class="personal-influence-item">
-            <div class="personal-influence-num color1">5411</div>
-            <div class="personal-influece-label">总访问量</div>
-          </div>
-          <div class="personal-influence-item">
-            <div class="personal-influence-num color2">913</div>
-            <div class="personal-influece-label">粉丝</div>
-          </div>
-          <div class="personal-influence-item">
-            <div class="personal-influence-num color3">72</div>
-            <div class="personal-influece-label">作品</div>
-          </div>
-        </div>
-        <el-tabs v-model="activeName" class="personal-tabs">
-          <el-tab-pane label="最新作品" name="first">
-            <div class="content">How to Contribute to Open Source?</div>
-          </el-tab-pane>
-          <el-tab-pane label="最热作品" name="second">
-            <div class="content">为什么程序员们愿意在GitHub上开源...</div>
-          </el-tab-pane>
-        </el-tabs>
+        占位
       </div>
       <div class="article">
         <div class="article-title">文章</div>
-        <div class="article-list">
-          <div class="article-item" @click="handleArticle('https://opensource.guide/how-to-contribute/')">
-            <img class="article-thumb" src="../../assets/img/about/open-source.jpg" alt="" />
-            <div class="article-detail">
-              <p class="article-detail-title">How to Contribute to Open Source?</p>
-              <div class="article-detail-content">
-                Whether you just made your first open source contribution, or you’re looking for new ways to contribute,
-                we hope you’re inspired to take action. Even if your contribution wasn’t accepted, don’t forget to say
-                thanks when a maintainer put effort into helping you. Open source is made by people like you: one issue,
-                pull request, comment, or high-five at a time.
-              </div>
-              <div class="article-tool">
-                <div class="pubdate">一天前</div>
-                <div class="article-about">
-                  <span><i class="iconfont icon-shoucang"></i>37</span>
-                  <el-divider direction="vertical"></el-divider>
-                  <span><i class="iconfont icon-pinglun"></i>2384</span>
-                  <el-divider direction="vertical"></el-divider>
-                  <span><i class="iconfont icon-fenxiang"></i>56</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="article-item" @click="handleArticle('https://www.zhihu.com/question/269033309')">
-            <img class="article-thumb" src="../../assets/img/about/open-source.jpg" alt="" />
-            <div class="article-detail article-last">
-              <p class="article-detail-title">为什么程序员们愿意在GitHub上开源自己的成果给别人免费使用和学习？</p>
-              <div class="article-detail-content">
-                “Git的精髓在于让所有人的贡献无缝合并。而GitHub的天才之处，在于理解了Git的精髓。”来一句我们程序员们接地气的话：分享是一种快乐~
-              </div>
-              <div class="article-tool">
-                <div class="pubdate">2019年5月26日</div>
-                <div class="article-about">
-                  <span><i class="iconfont icon-shoucang"></i>37</span>
-                  <el-divider direction="vertical"></el-divider>
-                  <span><i class="iconfont icon-pinglun"></i>2384</span>
-                  <el-divider direction="vertical"></el-divider>
-                  <span><i class="iconfont icon-fenxiang"></i>56</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -206,7 +157,9 @@ export default {
       .welcome {
         margin: 28px 0 0 30px;
         .welcome-title {
-          width: 366px;
+          width: 500px;
+          color: #fff;
+          font-size: 35px;
           height: 31px;
         }
         .subtitle {
@@ -224,11 +177,13 @@ export default {
             margin-top: 6px;
             width: 160px;
             height: 22px;
-            background: rgba(44, 95, 233, 1);
+            background: #2c5fe9;
             border-radius: 11px;
             text-align: center;
             line-height: 20px;
-            color: rgba(255, 255, 255, 1);
+            color: #fff;
+            font-size: 14px;
+            padding: 10px 10px;
           }
         }
       }
@@ -289,7 +244,7 @@ export default {
 
               .team-role {
                 display: inline-block;
-                width: 30px;
+                width: 50px;
                 margin-right: 15px;
                 font-weight: 400;
                 color: #45526b;
@@ -406,60 +361,6 @@ export default {
       background: rgba(255, 255, 255, 1);
       box-shadow: 0px 2px 14px 0px rgba(243, 243, 243, 1);
       border-radius: 8px;
-      .personal-title {
-        margin: 20px 0 10px 20px;
-        height: 22px;
-        line-height: 22px;
-        font-weight: 500;
-        color: #596c8e;
-        font-size: 16px;
-      }
-      .personal-avatar {
-        width: 140px;
-        height: 140px;
-        margin: 0 auto 40px;
-        border-radius: 75px;
-        box-shadow: 0 0 30px 0 #cfd5e3;
-      }
-      .personal-influence {
-        display: flex;
-        justify-content: space-between;
-        padding: 0 30px 40px;
-        .personal-influence-item {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          .personal-influence-num {
-            font-size: 28px;
-            line-height: 34px;
-            &.color1 {
-              color: #00c292;
-            }
-            &.color2 {
-              color: #fec108;
-            }
-            &.color3 {
-              color: #03a9f3;
-            }
-          }
-          .personal-influece-label {
-            font-size: 12px;
-            font-weight: 400;
-            color: #8c98ae;
-            line-height: 17px;
-          }
-        }
-      }
-      .personal-tabs {
-        margin-bottom: 20px;
-      }
-      .personal-tabs /deep/ .is-top {
-        width: 320px;
-        display: flex;
-        justify-content: space-around;
-      }
-      .personal-tabs /deep/ .el-tabs__content {
-        text-indent: 20px;
       }
     }
     .article {
@@ -469,73 +370,8 @@ export default {
       background: rgba(255, 255, 255, 1);
       box-shadow: 0px 2px 14px 0px rgba(243, 243, 243, 1);
       border-radius: 8px;
-      .article-title {
-        height: 22px;
-        line-height: 22px;
-        font-weight: 500;
-        color: #596c8e;
-        font-size: 16px;
-        margin-bottom: 20px;
-      }
-      .article-list {
-        cursor: pointer;
-        .article-item {
-          display: flex;
-          flex-direction: row;
-          justify-content: flex-start;
-
-          .article-thumb {
-            width: 120px;
-            height: 120px;
-            border-radius: 8px;
-            margin-right: 30px;
-          }
-          .article-detail {
-            flex: 1;
-            border-bottom: 1px #ecedef solid;
-            margin-bottom: 20px;
-            &.article-last {
-              border-bottom: none;
-              margin-bottom: 0;
-            }
-            .article-detail-title {
-              height: 22px;
-              font-size: 16px;
-              font-weight: 400;
-              color: rgba(69, 82, 107, 1);
-              line-height: 22px;
-            }
-            .article-detail-content {
-              margin-top: 10px;
-              font-size: 14px;
-              font-weight: 400;
-              color: rgba(140, 152, 174, 1);
-              line-height: 22px;
-            }
-          }
-          .article-tool {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            margin: 10px 0 20px 0;
-            font-size: 12px;
-            line-height: 17px;
-            font-weight: 400;
-            color: #808da3;
-            .article-about {
-              .iconfont {
-                line-height: 17px;
-                margin-right: 6px;
-                font-size: 12px;
-              }
-            }
-          }
-        }
-      }
     }
   }
-}
-
 @media screen and (max-width: 1200px) {
   .container .lin-info .lin-info-right {
     display: none;
